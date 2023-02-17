fe15db5cebee7fc7a8a1d96913d9d3c538cb9bd8
Yari Gomez <ygomez1@luc.edu>
2023-02-17 17:06:14 -0600

    shortened DNA.py with a for loop

--- DNA.py
+++ DNA.py
@@ -1,7 +1,3 @@
-hi
 s = open('rosalind_dna.txt','r').read()
-a = s.count("A")
-c = s.count("C")
-g = s.count("G")
-t = s.count("T")
-print(a, c, g, t)
+for n in ["A","C","G","T"]:
+    print(s.count(n), end=' ')
